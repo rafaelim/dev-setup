d28689e98c0a6cdb4a8191476179b367066a0566
Rafael Lima <rafael.lima@lumenalta.com>
2026-03-11 15:32:39 -0400

    fix: add extra validation to xrandr script

--- env/personal/.xrandr.sh
+++ env/personal/.xrandr.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-xrandr --output HDMI-0 --mode 1920x1080 --pos 0x360 --rotate normal --output DP-0 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off
+
+if xrandr | grep -q "^DP-0 connected" && xrandr | grep -q "^HDMI-0 connected"; then
+    xrandr --output HDMI-0 --mode 1920x1080 --pos 0x360 --rotate normal \
+           --output DP-0 --primary --mode 2560x1440 --pos 1920x0 --rotate normal \
+           --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off
+fi
